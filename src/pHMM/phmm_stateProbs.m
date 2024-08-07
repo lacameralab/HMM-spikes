a9e7948a21f52707da053e8b431603cc200e8406
Tianshu Li <71147060+TianshuLi-sbu@users.noreply.github.com>
2024-08-07 10:54:09 -0400

    Update phmm_stateProbs.m
    Add the choice of setting the initial state probability to be delta function (1,0,...,0).

--- src/pHMM/phmm_stateProbs.m
+++ src/pHMM/phmm_stateProbs.m
@@ -5,23 +5,24 @@ function [stateprobs] = phmm_stateProbs(x,m,lambda,Gamma,delta)
 %   x: observation matrix
 %   m: number of states
 %   lambda: firing rate matrix
-%   Gamma: transition matrix
-%   delta: stationary distribution (optional)
+%   Gamma: transition probability matrix
+%   delta: initial state distribution (optional)
 % Outputs:
-%   stateprobs: matrix, each row is the probability of one state at each
+%   stateprobs: matrix, each row is the probability of staying in one state at a
 %       time bin.
 % 
-% Tianshu Li
+% - Tianshu Li
 
 if nargin < 5
-    delta = abs(ones(1,m)/(eye(m)-Gamma+ones(m)));
+    delta = abs(ones(1,m)/(eye(m)-Gamma+ones(m))); % set initial state distribution to stationary distribution
+    % delta = zeros(1,m); delta(1) = 1; % assume every trial starts at state 1
 end
 
-n = size(x,2);
-[la, lb] = phmm_lalphabeta(m,x,lambda,Gamma,delta);
+n = size(x,2); % number of time bins
+[la, lb] = phmm_lalphabeta(m,x,lambda,Gamma,delta); % forward and backward probabilities: log(alpha), log(beta)
 
 c = max(la(:,n));
-llk = c+log(sum(exp(la(:,n)-c)));
+llk = c+log(sum(exp(la(:,n)-c))); % loglikelihood
 stateprobs = zeros(m,n);
 for i = 1:n
     stateprobs(:,i) = exp(la(:,i)+lb(:,i)-llk);
