41c66e2228887990607375fdc759e63ce51ba63b
Tianshu Li <71147060+TianshuLi-sbu@users.noreply.github.com>
2024-11-01 23:11:27 -0400

    Add files via upload
    Edited the comments in this function (removed the comments from hmmdecode.m)

--- src/pHMM/phmm_decode.m
+++ src/pHMM/phmm_decode.m
@@ -1,116 +1,46 @@
-function [pStates,pSeq, fs, bs, s] = phmm_decode(x,Gamma,lambda,varargin)
-% phmm_decode is the Poisson HMM version of MATLAB function hmmdecode.
-% Gamma is tr in hmmdecode, P_xt is e in hmmdecode.
-% Gamma must be square
+function [pStates,logpSeq] = phmm_decode(x,Gamma,lambda)
+% [pStates,logpSeq] = phmm_decode(x,Gamma,lambda,varargin)
 % 
-% HMMDECODE calculates the posterior state probabilities of a sequence.
-%   PSTATES = HMMDECODE(SEQ,TRANSITIONS,EMISSIONS) calculates the posterior
-%   state probabilities, PSTATES, of sequence SEQ from a Hidden Markov
-%   Model specified by transition probability matrix,  TRANSITIONS, and
-%   EMISSION probability matrix, EMISSIONS. TRANSITIONS(I,J) is the
-%   probability of transition from state I to state J. EMISSIONS(K,SYM) is
-%   the probability that symbol SYM is emitted from state K. The posterior
-%   probability of sequence SEQ is the probability P(state at step i = k |
-%   SEQ).  PSTATES is an array with the same length as SEQ and one row for
-%   each state in the model. The (i,j) element of PSTATES gives the
-%   probability that the model was in state i at the jth step of SEQ.
-%
-%   [PSTATES, LOGPSEQ] = HMMDECODE(SEQ,TR,E) returns, LOGPSEQ, the log of
-%   the probability of sequence SEQ given transition matrix, TR and
-%   emission matrix, E.
-%
-%   [PSTATES, LOGPSEQ, FORWARD, BACKWARD, S] = HMMDECODE(SEQ,TR,E) returns
-%   the forward and backward probabilities of the sequence scaled by S. The
-%   actual forward probabilities can be recovered by using:
-%        f = FORWARD.*repmat(cumprod(s),size(FORWARD,1),1);
-%   The actual backward probabilities can be recovered by using:
-%       bscale = cumprod(S, 'reverse'); b =
-%       BACKWARD.*repmat([bscale(2:end), 1],size(BACKWARD,1),1);
-%
-%   HMMDECODE(...,'SYMBOLS',SYMBOLS) allows you to specify the symbols that
-%   are emitted. SYMBOLS can be a numeric array or a string array cell
-%   array of the names of the symbols.  The default symbols are integers 1
-%   through M, where N is the number of possible emissions.
-%
-%   This function always starts the model in state 1 and then makes a
-%   transition to the first step using the probabilities in the first row
-%   of the transition matrix. So in the example given below, the first
-%   element of the output states will be 1 with probability 0.95 and 2 with
-%   probability .05.
-%
-%   Examples:
-%
-%       tr = [0.95,0.05; ...
-%             0.10,0.90];
-%           
-%       e = [1/6,  1/6,  1/6,  1/6,  1/6,  1/6; ...
-%            1/10, 1/10, 1/10, 1/10, 1/10, 1/2;];
-%
-%       [seq, states] = hmmgenerate(100,tr,e); 
-%       pStates = hmmdecode(seq,tr,e);
-%
-%       [seq, states] = hmmgenerate(100,tr,e,'Symbols',...
-%                 {'one','two','three','four','five','six'});
-%       pStates = hmmdecode(seq,tr,e,'Symbols',...
-%                 {'one','two','three','four','five','six'});
-%
-%   See also HMMGENERATE, HMMESTIMATE, HMMVITERBI, HMMTRAIN.
+% phmm_decode calculates the posterior state probabilities of a spike count
+% sequence given a Poisson HMM model defined by transition probability
+% matrix Gamma and firing rate matrix lambda.
+% N is the number of neurons, T is the number of time bins, and m is the
+% number of states.
+% Inputs:
+%   x: spike count sequence (NxT)
+%   Gamma: transition probability matrix (mxm)
+%   lambda: firing rate matrix (Nxm), the unit is number of spikes in a
+%       time bin. 
+% Outputs:
+%   pStates: posterior state probabilities
+%   logpSeq: log of the probability of the whole sequence
 % 
-%   Reference: Biological Sequence Analysis, Durbin, Eddy, Krogh, and
-%   Mitchison, Cambridge University Press, 1998.
-% 
-%   Copyright 1993-2014 The MathWorks, Inc.
-
-if nargin > 0
-    if isstring(x)
-        x = cellstr(x);
-    end
-end
-
-if nargin > 3
-    [varargin{:}] = convertStringsToChars(varargin{:});
-end
+% Modified code from Matlab's function HMMDECODE.
 
 numStates = size(Gamma,1);
-checkTr = size(Gamma,2);
-if checkTr ~= numStates
-    error(message('stats:hmmdecode:BadTransitions'));
+checkGamma = size(Gamma,2);
+if checkGamma ~= numStates
+    error('Transition probability matrix (Gamma) must be square.');
 end
 
 % number of columns of lambda must be same as number of states
-
 checklambda  = size(lambda,2);
 if checklambda ~= numStates
-    error(message('stats:hmmdecode:InputSizeMismatch'));
+    error('The firing rate matrix (lambda) must have the same number of states as the transition probability matrix (Gamma).');
 end
 
 % add extra data to start to make algorithm cleaner at f0 and b0
 x = [zeros(size(lambda,1),1), x ];
 T = size(x,2);
 
-% P(xt) whis is e in hmmdecode
+% P(xt)
 P_xt = zeros(numStates,T); % P(xt), p(i,t) = P(xt|Ci)
 for i = 1:numStates
     P_xt(i,:) = exp(sum(-lambda(:,i)+log(lambda(:,i)).*x-log(factorial(x)),1));
-%     P_xt(i,:) = exp(sum(-lambda(:,i)+log(lambda(:,i)).*x)); % it is not necessary to include the factorial term in the observation density because it is common to all states.
 end
 
-% This is what we'd like to do but it is numerically unstable
-% warnState = warning('off');
-% logTR = log(tr);
-% logE = log(e);
-% warning(warnState);
-% f = zeros(numStates,L);
-% f(1,1) = 1;
-% % for count = 2:L
-%     for state = 1:numStates
-%         f(state,count) = logE(state,seq(count)) + log(sum( exp(f(:,count-1) + logTR(:,state))));
-%     end
-% end
-% f = exp(f);
-
-% so we introduce a scaling factor
-fs = zeros(numStates,T);
+% introduce a scaling factor for alpha (forward)
+fs = zeros(numStates,T); % scaled alpha
 fs(1,1) = 1;  % assume that we start in state 1.
 s = zeros(1,T);
 s(1) = 1;
@@ -123,20 +53,8 @@ for count = 2:T
     fs(:,count) =  fs(:,count)./s(count);
 end
 
-%  The  actual forward and  probabilities can be recovered by using
-%   f = fs.*repmat(cumprod(s),size(fs,1),1);
-
-
-% This is what we'd like to do but it is numerically unstable
-% b = zeros(numStates,L);
-% for count = L-1:-1:1
-%     for state = 1:numStates
-%         b(state,count) = log(sum(exp(logTR(state,:)' + logE(:,seq(count+1)) + b(:,count+1)  )));
-%     end
-% end
-
-% so once again use the scale factor
-bs = ones(numStates,T);
+% use the scale factor for beta (backward)
+bs = ones(numStates,T); % scaled beta
 for count = T-1:-1:1
     for state = 1:numStates
       bs(state,count) = (1/s(count+1)) * sum( Gamma(state,:)'.* bs(:,count+1) .* P_xt(:,count+1)); 
@@ -147,10 +65,11 @@ end
 %  scales = cumprod(s, 'reverse'); 
 %  b = bs.*repmat([scales(2:end), 1],size(bs,1),1);
 
-pSeq = sum(log(s));
+logpSeq = sum(log(s));
 pStates = fs.*bs;
 
 % get rid of the column that we stuck in to deal with the f0 and b0 
 pStates(:,1) = [];
 
+end
 
